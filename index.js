51b4f917ce417d445dabb2795a5284c7bf155c3e
IVAN RODRIGUEZ RAMOS <TLJ0149@municipio.tlajomulco.gob.mx>
2022-08-17 15:40:25 -0500

    ruta inicial añadido y modificado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res=> res.setEncoding("Hola Mundo")))
+app.get('/', (req, res=> res.send("Hola Mundo")))
 
 app.listen(process.env.PORT || 3000)
 console.log('Server on port', process.env.PORT || 3000)
