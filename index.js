ddd4ea7815c5a502c2b1933f7c28259bb11d2d3a
IVAN RODRIGUEZ RAMOS <TLJ0149@municipio.tlajomulco.gob.mx>
2022-08-18 08:35:13 -0500

    template añadido y api rest

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+
 
 const app = express()
 
-app.get('/', (req, res) => res.send("Hola Mundo"))
+// configuracion
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-//app.use(express.static('public'))
+// rutas
+app.get('/api/users', (req, res) => res.json([{name: 'ryan'}, {name: "jeo"}]))
 
+app.get('/profile', (req, res) => {
+    res.render('profile', {
+        name: 'Ivan',
+        age: 28
+    })
+})
+
+//archivos estaticos
+app.use(express.static('public'))
+
+//inicio servidor
 app.listen(process.env.PORT || 3000)
 console.log('Server on port', process.env.PORT || 3000)
 
